fbc38ff412b46168a4805d7e4616049c2736d807
Chrisfaherty <chrismactom@gmail.com>
2023-01-27 19:38:08 +0000

    Vid 7 changing models

--- vidly/movies/models.py
+++ vidly/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Genre(models.Model):
@@ -11,4 +12,5 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
 
