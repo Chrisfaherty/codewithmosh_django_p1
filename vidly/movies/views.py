564f5222d2597535d41104d6622cc2f0612f08dc
Chrisfaherty <chrismactom@gmail.com>
2023-01-29 15:27:24 +0000

    Vid 11 Templates

--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -7,6 +7,5 @@ from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
-    return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
 
